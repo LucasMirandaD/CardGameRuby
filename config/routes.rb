32fed10e7d93570abf5d0f27fa0f377aa4f01db5
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-10-06 23:24:43 -0300

    Rubocop routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,25 +4,26 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :players, param: :nickname, only: %i[update destroy new edit] do
+  resources :players, param: :nickname, only: %i[update destroy create edit] do
     member do
       post :login # /players/:nickname/login
     end
   end
 
-  resources :boards, only: %i[update destroy new edit] do
+  resources :boards, only: %i[update destroy create edit] do
   end
 
   # ROUTES
   # Prefix Verb  URI    Pattern                                                                                       Controller#Action
   # login_player POST   /players/:nickname/login(.:format)                                                                players#login
-  #   new_player GET    /players/new(.:format)                                                                            players#new
+  #      players POST   /players(.:format)                                                                                players#create
   #  edit_player GET    /players/:nickname/edit(.:format)                                                                 players#edit
   #       player PATCH  /players/:nickname(.:format)                                                                      players#update
   #              PUT    /players/:nickname(.:format)                                                                      players#update
   #              DELETE /players/:nickname(.:format)                                                                      players#destroy
-  #    new_board GET    /boards/new(.:format)                                                                             boards#new
+  #       boards POST   /boards(.:format)                                                                                 boards#create
   #   edit_board GET    /boards/:id/edit(.:format)                                                                        boards#edit
   #        board PATCH  /boards/:id(.:format)                                                                             boards#update
   #              PUT    /boards/:id(.:format)                                                                             boards#update
+  #              DELETE /boards/:id(.:format)                                                                             boards#destroy
 end
